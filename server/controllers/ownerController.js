eddf0129774d6d83700a2d802dd9735bae257d59
Sneh Yadav <snehyadav091@gmail.com>
2025-06-30 00:01:18 +0530

    Add booking functionality with availability checks and user booking management
    - Implement booking model and controller for creating and managing bookings
    - Add routes for checking car availability, creating bookings, and retrieving user and owner bookings
    - Enhance owner dashboard with booking statistics and revenue calculations
    - Include user image update functionality in owner routes

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -3,6 +3,7 @@ import imagekit from "../configs/imageKit.js";
 import Car from "../models/car.js";
 import User from "../models/user.js";
 import fs from "fs";
+import Booking from "../models/booking.js";
 
 // api to change role to user
 export const changeRoleToOwner = async (req, res) => {
@@ -118,8 +119,74 @@ export const getDashboardData = async (req, res) => {
       return res.json({ success: false, message: "Unauthorized" });
     }
 
-    const cars = await Car.find({owner: _id})
-    
+    const cars = await Car.find({ owner: _id });
+    const bookings = await Booking.find({ owner: _id })
+      .populate("car")
+      .sort({ createdAt: -1 });
+
+    const pendingBookings = await Booking.find({
+      owner: _id,
+      status: "pending",
+    });
+    const completedBookings = await Booking.find({
+      owner: _id,
+      status: "confirmed",
+    });
+
+    // calculate monthlyRevenue from booking where status is confirmed
+
+    const monthlyRevenue = bookings
+      .slice()
+      .filter((booking) => booking.status === "confirmed")
+      .reduce((acc, (booking) => acc + booking.price, 0));
+
+    const dashboardData = {
+      totalCars: cars.length,
+      totalBookings: bookings.length,
+      pendingBookings: pendingBookings.length,
+      completedBookings: completedBookings.length,
+      recentBookings: bookings.slice(0, 3),
+      monthlyRevenue,
+    };
+
+    res.json({ success: true, dashboardData });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// api to update user image
+
+export const updateUserImage = async (req, res) => {
+  try {
+    const { _id } = req.user;
+
+    const imageFile = req.file;
+
+    // upload image to imageKit
+    const fileBuffer = fs.readFileSync(imageFile.path);
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/users",
+    });
+
+    // For URL Generation, works for both images and videos
+    var optimizedImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        { width: "400" }, // width resizing
+        { quality: "auto" }, // auto compression
+        { format: "webp" }, // convert to modern format
+      ],
+    });
+
+    const image = optimizedImageUrl;
+
+    await User.findByIdAndUpdate(_id, {image});
+    res.json({success: true, message: "Image Updated"})
+
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
