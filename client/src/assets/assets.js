decf2e06049f648d4d0f87f786982dc711fee09a
Sneh Yadav <snehyadav091@gmail.com>
2025-07-05 18:29:39 +0530

    Add 'Manavadar' to cityList in assets.js

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -43,7 +43,7 @@ import car_image2 from "./car_image2.png"
 import car_image3 from "./car_image3.png"
 import car_image4 from "./car_image4.png"
 
-export const cityList = ['Gandhinagar','Ahemdabad' , 'Morbi', 'Jamnagar', 'Himatnagar']
+export const cityList = ['Gandhinagar','Ahemdabad' , 'Morbi', 'Jamnagar', 'Himatnagar', 'Manavadar']
 
 export const assets = {
     logo,
